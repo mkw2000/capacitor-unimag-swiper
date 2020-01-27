18b9db63a90a0a2a36705c60557369a42d094cc7
mkw <michaelkweiner@gmail.com>
2020-01-27 10:40:02 -0800

    please work

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -1,6 +1,7 @@
 #import <Foundation/Foundation.h>
 #import <Capacitor/Capacitor.h>
 #import <UIKit/UIKit.h>
+#import <Plugin/uniMag.h>
 
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
