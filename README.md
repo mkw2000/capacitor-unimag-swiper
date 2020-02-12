36bf2d83168b88ee7f521c934681ac3dc4c71c05
mkw <michaelkweiner@gmail.com>
2020-02-12 14:36:12 -0800

    fixed events

--- README.md
+++ README.md
@@ -1,2 +1,135 @@
-# capacitor-unimag-swiper
-Capacitor plugin for IDTech mobile credit card swipers
+# Capacitor Unimag Swiper
+
+The purpose of this plugin is to allow for swiping credit cards using the IDTech UniMag II, UniMag Pro, or Shuttle mobile readers in hybrid applications.
+
+### Supported Platforms
+
+- **iOS**
+
+### Installation
+
+```
+npm install capacitor-unimag-swiper
+```
+
+```
+npx cap sync ios 
+```
+
+
+## Usage
+
+The IDTech reader can be used by calling the **activate** method on your plugin object. Here, "activated" means that the plugin will be able to listen to IDTech SDK events. The IDTech SDK (hereby referred to simply as SDK) sends a wide variety of events related to reader tasks such as connection and swipe. In response to some of these events, the plugin will fire its own events that you can capture in your application.
+
+A reader may be either **attached** or **detached**. It is attached when it is plugged into the mobile device running your application. While the reader is activated, once a reader is attached, (or if it is already attached upon application startup) the SDK will begin a connection task with the reader. When this task begins, a **"connecting"** event will be fired from the plugin. After the task has finished successfully, a **"connected"** event will fire. If a connection could not be established, a **"timeout"** event will be fired instead. Once you have captured the **"connected"** event you can begin initiating the swipe process.
+
+To initiate the swipe process, call the **swipe** method on your plugin object. After this method has been called you can physically swipe the card. The data will be parsed and returned if valid. This will result in a **"swipe_success"** event containing the data (it will be stringified, you will need to parse it). If the card data was invalid, or the swipe was otherwise unsuccessful (e.g., if it was crooked) you a **"swipe_error"** event will be fired instead.
+
+You can deactivate the reader by calling the **deactivate** method on your plugin object. Once the reader has been deactivated, it will not listen to attachment/detachment and will never attempt a connection. The reader need not be attached for it to be activated successfully - if it is activated, it will automatically detect attachment/detachment and handle connection as such.
+
+NOTE: To use this plugin you'll need to disable bitcode. You can do this by clicking on your project in Xcode and going to Build Settings. Search for 'bitcode', and you'l see an 'Enable Bitcode' setting. Change this to 'No'. There is currently no way for me to configure this through the plugin, as far as I'm aware.
+
+## Events
+
+See Sample section for how exactly to capture the events listed below.
+
+| name                 | platform     | meaning                                                                                                                                            | details                                                                                                                                                                   |
+| -------------------- | ------------ | -------------------------------------------------------------------------------------------------------------------------------------------------- | ------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| connecting           | iOS, Android | connection task has begun                                                                                                                          | none                                                                                                                                                                      |
+| connected            | iOS, Android | connection task was successful                                                                                                                     | none                                                                                                                                                                      |
+| disconnected         | iOS, Android | reader was disconnected                                                                                                                            | none                                                                                                                                                                      |
+| timeout              | iOS, Android | connection or swipe task has timed out                                                                                                             | string: message from SDK regarding timeout type                                                                                                                           |
+| swipe_processing     | iOS, Android | swipe has been received and is processing                                                                                                          | none                                                                                                                                                                      |
+| swipe_success        | iOS, Android | card data has been parsed successfully                                                                                                             | string: use JSON.parse to get object of card data w/ properties card_number, expiry_month, expiry_year, first_name, last_name, & trimmedUnimagData (raw data from reader) |
+| swipe_error          | iOS, Android | card data was invalid and could not be parsed                                                                                                      | none                                                                                                                                                                      |
+| connection_error     | iOS          | connection task was unsuccessful                                                                                                                   | string: message from plugin with reason reader could not connect                                                                                                          |
+| xml_error            | Android      | xml config file listing settings for devices could not be loaded, can also be called during auto config if the device's volume could not be raised | string: message from SDK regarding particular issue with XML config file                                                                                                  |
+| autoconfig_completed | Android      | auto config process completed and connection should now begin                                                                                      | none                                                                                                                                                                      |
+| autoconfig_error     | Android      | auto config process failed or timed out                                                                                                            | string: message from SDK in case of timeout                                                                                                                               |
+
+## Sample
+
+The sample demonstrates how to activate the reader, capture events, and swipe a card.
+
+```javascript
+document.addEventListener(
+  "deviceready",
+  function() {
+    cordova.plugins.unimag.swiper.activate();
+    cordova.plugins.unimag.swiper.enableLogs(true);
+    cordova.plugins.unimag.swiper.setReaderType("unimag_ii");
+
+    var connected = false;
+
+    var swipe = function() {
+      if (connected) {
+        cordova.plugins.unimag.swiper.swipe(
+          function successCallback() {
+            console.log("SUCCESS: Swipe started.");
+          },
+          function errorCallback() {
+            console.log("ERROR: Could not start swipe.");
+          }
+        );
+      } else console.log("ERROR: Reader is not connected.");
+    };
+
+    cordova.plugins.unimag.swiper.on("connected", function() {
+      connected = true;
+    });
+
+    cordova.plugins.unimag.swiper.on("disconnected", function() {
+      connected = false;
+    });
+
+    cordova.plugins.unimag.swiper.on("swipe_success", function(e) {
+      var data = JSON.parse(e.detail);
+      console.log("cardholder name: " + data.first_name + " " + data.last_name);
+      console.log("card number:" + data.card_number);
+      console.log("expiration:" + data.expiry_month + "/" + data.expiry_year);
+    });
+
+    cordova.plugins.unimag.swiper.on("swipe_error", function() {
+      console.log("ERROR: Could not parse card data.");
+    });
+
+    cordova.plugins.unimag.swiper.on("timeout", function(e) {
+      if (connected) {
+        console.log("ERROR: Swipe timed out - " + e.detail);
+      } else {
+        console.log("ERROR: Connection timed out - " + e.detail);
+      }
+    });
+  },
+  false
+); 
+
+
+    async activate() {
+        const result = await CapacitorUnimagSwiper.activateReader();
+        if (result === "active") {
+            this.activated = true;
+        } else {
+
+        }
+    }
+
+    async ngOnInit() {
+    
+    
+    
+    }
+
+```
+
+## Contributing (please do!)
+
+1. Fork this repo.
+2. Create your own branch. (`git checkout -b my-feature`)
+3. Commit your changes. (`git commit -am 'Add my awesome feature'`)
+4. Push to your branch. (`git push origin my-feature`)
+5. Create new pull request.
+
+## License
+
+This software is released under the <a href="http://www.wtfpl.net/">WTFPL License</a>.
--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -4,12 +4,12 @@ import UIKit
 
 @objc(CapacitorUnimagSwiper)
 public class CapacitorUnimagSwiper: CAPPlugin {
-    
+
     /**
      * This plugin allows for swiping a credit or debit card and returning
      * its parsed data for use in financial transactions.
      */
-    
+
     // Indicates whether app has launched and observers have been set
     var pluginInited = false
     // Reader from SDK to handle all swipe functionality
@@ -20,61 +20,61 @@ public class CapacitorUnimagSwiper: CAPPlugin {
     var enableLogs = true
     // Type of uniMag reader
     var readerType: UmReader?
-    
+
     /***************************************************
      * LIFECYCLE
      ***************************************************/
-    
+
     /**
      * Rather than register an observer for UIApplicationDidFinishLaunchingNotification,
      * this method is used directly to register observers for subsequent lifecycle
      * notifications.
      */
-    
+
     @objc func initPlugin() {
         if !pluginInited {
             uniMag.enableLogging(true)
-            
+
             let center = NotificationCenter.default
-            
+
             center.addObserver(self, selector: #selector(onPause), name:
                 UIApplication.willResignActiveNotification, object: nil)
-            
+
             center.addObserver(self, selector: #selector(onResume), name:
                 UIApplication.didBecomeActiveNotification, object: nil)
-            
+
             pluginInited = true
         }
     }
-    
+
     /**
      * Called when the application enters the background.
      * The reader is killed to maintain consistency with Android behavior.
      */
-    
+
     @objc func onPause(_ notification: Notification?) {
         if readerActivated {
             //            deactivateReader(nil)
         }
     }
-    
-    
+
+
     /**
      * Called when the application returns to the foreground.
      * The reader is reinitialized as if the app was just opened.
      */
-    
+
     @objc func onResume(_ notification: Notification?) {
         if readerActivated {
             activateReader(nil)
         }
     }
-    
-    
+
+
     /***************************************************
      * JAVASCRIPT INTERFACE IMPLEMENTATION
      ***************************************************/
-    
+
     /**
      * Initializes uniMag object to start listening to SDK events
      * for connection, disconnection, swiping, etc.
@@ -82,45 +82,45 @@ public class CapacitorUnimagSwiper: CAPPlugin {
      * @param {CDVInvokedUrlCommand*}
      *        The command sent from JavaScript
      */
-    
+
     @objc func activateReader(_ call: CAPPluginCall?) {
-        
+
         DispatchQueue.global(qos: .background).async {
-            
+
             // Register observers if this is the first time reader
             // is activated
             self.initPlugin()
-            
+
             if self.reader == nil {
                 // Because logging is a class method, we can call
                 // it before initialization
                 uniMag.enableLogging(self.enableLogs)
-                
+
                 // Begin listening to SDK events, including
                 // initialization
                 self.setReaderListener(true)
-                
+
                 self.reader = uniMag.init()
                 self.reader?.setAutoConnect(true)
                 self.reader?.setSwipeTimeoutDuration(30)
                 self.reader?.setAutoAdjustVolume(true)
-                
+
                 // Set type if possible
                 if self.readerType != nil {
                     self.reader?.readerType = self.readerType!
                 }
-                
+
                 // Store status of connection task
                 let activated = self.reader!.start(true)
-                
+
                 if call != nil {
                     self.readerActivated = true;
-                    
+
                     if activated == UMRET_SUCCESS {
                         call?.resolve([
                             "value": "activate reader",
                         ])
-                        
+
                     } else if activated == UMRET_NO_READER {
                         call?.resolve([
                             "value": "no reader",
@@ -132,36 +132,36 @@ public class CapacitorUnimagSwiper: CAPPlugin {
                     call?.reject("call is nil")
                 }
             }
-            
+
         }
-        
+
     }
-    
+
     @objc func deactivateReader(_ call: CAPPluginCall?) {
-        
+
         if self.reader != nil {
             self.reader?.cancelTask()
-            
+
             // Stop listening to SDK events
             self.setReaderListener(false)
             self.reader = nil
-            
+
             fireEvent("disconnected")
         } else {
             call?.resolve([
                 "value": "reader already deactivated",
             ])        }
-        
+
         if call != nil {
             self.readerActivated = false
             call?.resolve([
                 "value": "deactivate reader",
             ])
-            
+
         }
-        
+
     }
-    
+
     /**
      * Tells the SDK to begin expecting a swipe. From the moment this is
      * called, the user will have 30 seconds to swipe a card before a
@@ -170,20 +170,20 @@ public class CapacitorUnimagSwiper: CAPPlugin {
      * @param {CDVInvokedUrlCommand*} command
      *        The command sent from JavaScript
      */
-    
+
     @objc func swipe(_ call: CAPPluginCall?) {
-        
+
         DispatchQueue.global(qos: .background).async {
             print("This is run on the background queue")
             //            var result: CDVPluginResult?
-            
+
             if self.reader != nil {
                 guard let reader = self.reader else {
                     return
                 };
                 if reader.getConnectionStatus() == true {
                     reader.cancelTask()
-                    
+
                     // Store status of swipe task
                     let swipeStarted = reader.requestSwipe()
                     print(swipeStarted)
@@ -193,7 +193,7 @@ public class CapacitorUnimagSwiper: CAPPlugin {
                         ])
                     } else {
                         call?.reject("swipe fail")
-                        
+
                     }
                 } else {
                     call?.resolve([
@@ -205,125 +205,125 @@ public class CapacitorUnimagSwiper: CAPPlugin {
                     "value": "Reader must be activated before starting swipe.",
                 ])
             }
-            
+
         }
-        
+
     }
-    
-    
+
+
     /***************************************************
      * SDK CALLBACKS
      ***************************************************/
-    
+
     /**
      * Receive notification from the SDK when the device is powering up.
      * Can result in a timeout rather than an actual connection.
      */
-    
+
     @objc func umConnecting(_ notification: Notification?) {
-        fireEvent("connecting")
+        fireEvent("uniMagPoweringNotification")
     }
-    
+
     /**
      * Receive notification from the SDK when the swiper is connected to
      * the device. Swipe cannot be performed until this has been called.
      */
     @objc func umConnected(_ notification: Notification?) {
-        fireEvent("connected")
+        fireEvent("uniMagDidConnectNotification")
     }
-    
+
     /**
      * Receive notification from the SDK when the swiper becomes disconnected
      * from the device.
      */
     @objc func umDisconnected(_ notification: Notification?) {
-        fireEvent("disconnected")
+        fireEvent("uniMagDidDisconnectNotification")
     }
-    
+
     /**
      * Receive notification from the SDK when connection task has timed out.
      */
     @objc func umConnectionTimeout(_ notification: Notification?) {
-        fireEvent("timeout", withData: "Connection timed out.")
+        fireEvent("uniMagTimeoutSwipeNotification", withData: "Connection timed out.")
     }
-    
+
     /**
      * Receive notification from SDK when system volume is too low to connect.
      */
     @objc func umConnection_InsufficientPower(_ notification: Notification?) {
-        fireEvent("timeout", withData: "Volume too low. Please maximize volume before reattaching swiper.")
+        fireEvent("uniMagTimeoutSwipeNotification", withData: "Volume too low. Please maximize volume before reattaching swiper.")
     }
-    
+
     /**
      * Receive notification from SDK when mono audio is enabled by system,
      * blocking a connection.
      */
     @objc func umConnectionMonoAudio(_ notification: Notification?) {
-        fireEvent("timeout", withData: "Mono audio is enabled. Please disable it in your iOS settings.")
+        fireEvent("uniMagTimeoutSwipeNotification", withData: "Mono audio is enabled. Please disable it in your iOS settings.")
     }
-    
+
     /**
      * Receive notification from the SDK when swipe task has timed out.
      */
     @objc func umSwipeTimeout(_ notification: Notification?) {
-        fireEvent("timeout", withData: "Swipe timed out.")
+        fireEvent("uniMagTimeoutSwipeNotification", withData: "Swipe timed out.")
     }
-    
+
     /**
      * Receive notification from the SDK as soon as it detects data coming from
      * the swiper after requestSwipe API method is called.
      */
     @objc func umSwipeProcessing(_ notification: Notification?) {
-        fireEvent("swipe_processing")
+        fireEvent("uniMagDataProcessingNotification")
     }
-    
+
     /**
      * Receive notification from the SDK when it cannot read a swipe (i.e., a
      * crooked swipe) rather than behave as if no swipe was made.
      */
     @objc func umSwipeError(_ notification: Notification?) {
-        fireEvent("swipe_error")
+        fireEvent("uniMagInvalidSwipeNotification")
     }
-    
+
     /**
      * Receive notification from the SDK when a successful swipe was read. Parses
      * the raw card data and sends resulting JSON with event.
      */
     @objc func umSwipeReceived(_ notification: Notification?) {
-        
+
                 let data = notification?.object as? Data
-        
+
                 var cardData: String? = nil
                 if let data = data {
                     cardData = String(data: data, encoding: .ascii)
                 }
-        
+
                 let parsedCardData = parseCardData(cardData)
-        
+
                 if parsedCardData != "" {
                     print(parsedCardData)
-                    fireEvent("swipe_success", withData: parsedCardData)
+                    fireEvent("uniMagDidReceiveDataNotification", withData: parsedCardData)
                 } else {
-                    fireEvent("swipe_error")
+                    fireEvent("uniMagInvalidSwipeNotification")
                 }
     }
-    
+
     /***************************************************
      * UTILS
      ***************************************************/
-    
+
     /**
      * Adds or removes observers for SDK notifications.
      *
      * @param {BOOL} listen
      *        Whether to register
      */
-    
+
     @objc func setReaderListener(_ listen: Bool) {
-        
-        
+
+
         let center = NotificationCenter.default
-        
+
         if listen {
             center.addObserver(self, selector: #selector(umConnected(_:)), name: NSNotification.Name("uniMagDidConnectNotification"), object: nil)
             center.addObserver(self, selector: #selector(umDisconnected(_:)), name: NSNotification.Name("uniMagDidDisconnectNotification"), object: nil)
@@ -335,7 +335,7 @@ public class CapacitorUnimagSwiper: CAPPlugin {
             center.addObserver(self, selector: #selector(umSwipeTimeout(_:)), name: NSNotification.Name("uniMagTimeoutSwipeNotification"), object: nil)
             center.addObserver(self, selector: #selector(umSwipeError(_:)), name: NSNotification.Name("uniMagInvalidSwipeNotification"), object: nil)
             center.addObserver(self, selector: #selector(umConnecting(_:)), name: NSNotification.Name("uniMagPoweringNotification"), object: nil)
-            
+
         } else {
             center.removeObserver(self, name: NSNotification.Name("uniMagDidConnectNotification"), object: nil)
             center.removeObserver(self, name: NSNotification.Name("uniMagDidDisconnectNotification"), object: nil)
@@ -347,11 +347,11 @@ public class CapacitorUnimagSwiper: CAPPlugin {
             center.removeObserver(self, name: NSNotification.Name("uniMagDidReceiveDataNotification"), object: nil)
             center.removeObserver(self, name: NSNotification.Name("uniMagInvalidSwipeNotification"), object: nil)
             center.removeObserver(self, name: NSNotification.Name("uniMagPoweringNotification"), object: nil)
-            
+
         }
-        
+
     }
-    
+
     /**
      * Uses a regex to parse raw card data.
      * @param  {NSString*} data
@@ -359,36 +359,36 @@ public class CapacitorUnimagSwiper: CAPPlugin {
      * @return {NSString*}
      *         Stringified JSON representation of parsed card data
      */
-    
+
         @objc func parseCardData(_ data: String?) -> String? {
             var num: String?
             var name: [AnyHashable]?
             var exp: String?
-    
+
             var error: Error? = nil
             var cardParser: NSRegularExpression? = nil
             do {
                 cardParser = try NSRegularExpression(pattern: "%B(\\d+)\\^([^\\^]+)\\^(\\d{4})", options: [])
             } catch {
             }
-    
+
             let matches = cardParser?.matches(in: data ?? "", options: [], range: NSRange(location: 0, length: data?.count ?? 0))
-    
+
             if matches?.count != nil {
                 if let range = matches?[0].range(at: 1) {
                     num = (data as NSString?)?.substring(with: range)
                 }
-    
+
                 if let range = matches?[0].range(at: 2) {
                     name = ((data as NSString?)!.substring(with: range)).components(separatedBy: "/")
                 }
-    
+
                 if let range = matches?[0].range(at: 3) {
                     exp = (data as NSString?)?.substring(with: range)
                 }
-    
+
                 if num != nil && (name?.count ?? 0) >= 2 && name?[0] != nil && name?[1] != nil && exp != nil {
-                      
+
                     let firstName = name?[0] as! String;
                     let lastName = name?[1] as! String;
                     let cardData: [String : Any] = [
@@ -399,14 +399,14 @@ public class CapacitorUnimagSwiper: CAPPlugin {
                         "last_name" : lastName,
                         "trimmedUnimagData" : data?.components(separatedBy: CharacterSet.whitespacesAndNewlines).joined(separator: "")
                     ]
-    
+
                     return String(data: try! JSONSerialization.data(withJSONObject: cardData, options: []), encoding: .utf8)
                 }
             }
             return nil
         }
-    
-    
+
+
     /**
      * Retrieve error message corresponding to a particular UmRet value.
      *
@@ -415,7 +415,7 @@ public class CapacitorUnimagSwiper: CAPPlugin {
      * @return {NSString*}
      *         Corresponding error message
      */
-    
+
     @objc func getUmRetErrorMessage(_ ret: UmRet) -> String? {
         switch ret {
         case UMRET_NO_READER:
@@ -434,18 +434,18 @@ public class CapacitorUnimagSwiper: CAPPlugin {
             return nil
         }
     }
-    
+
     /**
      * Pass event to method overload.
      *
      * @param {NSString*} event
      *        The event name
      */
-    
+
     @objc func fireEvent(_ event: String?) {
         fireEvent(event, withData: nil)
     }
-    
+
     /**
      * Format and send event to JavaScript side.
      *
@@ -454,12 +454,12 @@ public class CapacitorUnimagSwiper: CAPPlugin {
      * @param {NSString*} data
      *        Details about the event
      */
-    
+
     @objc func fireEvent(_ event: String?, withData data: String?) {
         print("firing event")
         print(event)
         let dataArg = data != nil ? "','\(data ?? "")" : ""
         self.notifyListeners(event, data: ["data" : dataArg])
     }
-    
+
 }
